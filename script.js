743111aea1924207e5dcf875aedfd2b40d7a8368
Heitor Botelho <copiaomeunome>
2026-09-02 17:56:38 -0300

    adding some drawings of al

---
+++ script.js
@@ -0,0 +1,17 @@
+import {configuraTudo, desenhaLinhas,  atualizaVertices} from "./draw.js"
+
+const inputv = document.getElementById("vert");
+let valor = Number(inputv.value);
+
+const inputs = document.getElementById("scale");
+let scale = Number(inputs.value);
+
+const gl = await configuraTudo("canvas", valor, scale);
+async function loopPrincipal(){
+    valor = Number(inputv.value);
+    scale = Number(inputs.value);
+    atualizaVertices(gl, valor, scale);
+    if(valor>1) desenhaLinhas(gl, valor);
+    requestAnimationFrame(loopPrincipal);
+}
+requestAnimationFrame(loopPrincipal);
